84d7b520a23ca94506116d9afcdda01bc01a1c13
fazaram <fazaramanugraha@gmail.com>
2026-04-21 00:11:58 +0700

    update env

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -4,13 +4,11 @@ const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
 export const prisma =
   globalForPrisma.prisma ||
-  (new PrismaClient({
-    datasources: {
-      db: {
-        url: process.env.DATABASE_URL,
-      },
-    },
+  new PrismaClient({
+    datasourceUrl: process.env.DATABASE_URL,
     log: ["query"],
-  }) as any);
+  });
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
