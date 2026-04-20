1db06ab2b9c2ce122ff550a00e38e458c383fbe2
fazaram <fazaramanugraha@gmail.com>
2026-04-21 00:06:59 +0700

    fix: move any cast to whole constructor to bypass vercel ts check

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -4,13 +4,13 @@ const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
 export const prisma =
   globalForPrisma.prisma ||
-  new PrismaClient({
+  (new PrismaClient({
     datasources: {
       db: {
         url: process.env.DATABASE_URL,
       },
     },
     log: ["query"],
-  } as any);
+  }) as any);
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
